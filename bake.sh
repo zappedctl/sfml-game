e357fe65b28650e7450752b9b0d2f5709a1ded38
zappedctl <zappedctl@protonmail.com>
2022-03-09 21:37:43 +0100

    Updating Compile Script

--- bake.sh
+++ bake.sh
@@ -1,14 +1,19 @@
+#!/bin/bash
+
 PROJECT_NAME="sfml-game"
 LIBRARIES="-lsfml-graphics -lsfml-window -lsfml-system"
 
 if [[ $1 == "comp" ]]
 then
+  echo "Compiling..."
+  mkdir -p build
   rm -f build/*
-  g++ -c src/Main.cpp src/Classes/**.cpp
-  g++ *.o -o build/$PROJECT_NAME -I src/Headers -I src/Headers/Entities $LIBRARIES
+  g++ -c src/Main.cpp src/Classes/*.cpp --std=c++11
+  g++ *.o -o build/$PROJECT_NAME -I src/Headers $LIBRARIES
   rm *.o
 elif [[ $1 == "comph" ]]
 then
+  echo "Compiling headers..."
   g++ --std=c++11 src/Headers.h
 elif [[ $1 == "cc" ]]
 then
@@ -41,13 +46,18 @@ then
   echo "{" >> src/Classes/${2^}.cpp
   echo "" >> src/Classes/${2^}.cpp
   echo "}" >> src/Classes/${2^}.cpp
+
+  echo "Successfuly created class ${2^}!"
 elif [[ $1 == "rc" ]]
 then
   rm src/Headers/${2^}.h
   rm src/Classes/${2^}.cpp
+
+  echo "Successfuly removed class ${2^}!"
 elif [[ $1 == "run" ]]
 then
+  echo "Launching ${PROJECT_NAME}..."
   ./build/$PROJECT_NAME
 else
-  echo "No option provided!"
+  echo "No valid option provided!"
 fi
